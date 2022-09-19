54864524ae8b3d73354502e1380c19dc3a23760b
prikulkarni105 <priyankakulkarni105@gmail.com>
2022-09-19 18:57:57 +0530

    Tis is master branch adding first time project

---
+++ GitPractice/src/main/java/Project1/GitPractice/Test1.java
@@ -0,0 +1,11 @@
+package Project1.GitPractice;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+
+	@Test
+	public void demo1() {
+		System.out.println("demo project");
+	}
+}
---
+++ GitPractice/src/main/java/Project1/GitPractice/Test2.java
@@ -0,0 +1,10 @@
+package Project1.GitPractice;
+
+import org.testng.annotations.Test;
+
+public class Test2 {
+    @Test
+	public void demo2() {
+		System.out.println("another demo project");
+	}
+}
